58c326ede0dceaf62481e701a7720c49625e83cb
GrahamZen <931252924@qq.com>
2024-02-18 15:50:29 -0500

    add gamemanager

---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyFirstARGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        public GameObject snowballPrefab;
+        public GameObject snowflakePrefab;
+        public GameObject shieldPrefab;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
